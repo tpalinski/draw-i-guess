ff07b5b86629041f326bd00ca2ebf02d487e3cab
tpalinski <tpalinski@icloud.com>
2023-03-14 16:45:15 +0100

    Create basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello from ts!")
+import express from "express"
+import dotenv from "dotenv"
+import http from "http";
+
+dotenv.config()
+const port = process.env.PORT || 3001;
+
+
+const app = express()
+const server = http.createServer(app)
+
+
+server.listen(port, () => {
+    console.log(`App is running on port ${port}`)
+})
