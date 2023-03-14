e573ce27e38efb593a1f0a6cdf03f139e718b5cf
tpalinski <tpalinski@icloud.com>
2023-03-14 18:05:28 +0100

    Add static page serving

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import dotenv from "dotenv"
-import http from "http";
+import http from "http"
+import path from "path";
 
 dotenv.config()
 const port = process.env.PORT || 3001;
@@ -8,6 +9,14 @@ const port = process.env.PORT || 3001;
 
 const app = express()
 const server = http.createServer(app)
+
+app.use(express.static(path.join(__dirname, "../", "public")))
+
+app.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../", "public/index.html"))
+})
+
+
 
 
 server.listen(port, () => {
